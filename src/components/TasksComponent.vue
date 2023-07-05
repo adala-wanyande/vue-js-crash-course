a987fcdea41f16740a25c67fd797ccfb3c7ba941
benard-dev <benardwanyande@gmail.com>
2023-07-05 23:09:48 +0300

    Complete code along

---
+++ src/components/TasksComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div :key="task.id" v-for="task in tasks">
+        <TaskComponent @toggle-reminder="$emit('toggle-reminder', task.id)" @delete-task="$emit('delete-task', task.id)" :task="task">{{ task.text }}</TaskComponent>
+    </div>
+</template>
+
+<script>
+    import TaskComponent from './TaskComponent'
+
+    export default {
+        name: 'TasksComponent',
+        props: {
+            tasks: Array
+        },
+        components: {
+            TaskComponent
+        },
+        emits: ['delete-task', 'toggle-reminder']
+    }
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,28 @@
+import {createRouter, createWebHistory} from 'vue-router'
+import AboutView from '../views/AboutView'
+import HomeView from '../views/HomeView'
+
+const routes = [
+    {
+        path: '/',
+        name: 'HomeView',
+        component: HomeView
+    },
+    {
+        path: '/about',
+        name: 'AboutView',
+        component: AboutView
+    },
+    {
+        path: '/about',
+        name: 'AboutView',
+        component: AboutView
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
+})
+
+export default router 
